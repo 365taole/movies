95f1b99a36c07540fdffc8a04830854b1c6a28c1
index.html <chengzai520@vip.qq.com>
2017-01-17 13:29:38 +0800

    Create jump.php

---
+++ jump.php
@@ -0,0 +1,7 @@
+<?php  
+$url = $_POST['url']; 
+echo "<script language='javascript' 
+type='text/javascript'>";  
+echo "window.location.href='http://yun.zihu.tv/yunparse/index.php?url=$url'";  
+echo "</script>";  
+?> 
